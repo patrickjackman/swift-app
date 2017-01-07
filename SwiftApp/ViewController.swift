0e350ca857afcb8e060f7217110b23bf3de60ba2
Patrick Jackman <Patrick@Jessicas-MacBook-Pro.local>
2017-01-07 10:37:30 -0500

    Initial Commit

---
+++ SwiftApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftApp
+//
+//  Created by Patrick Jackman on 1/7/17.
+//  Copyright © 2017 Patrick Jackman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
